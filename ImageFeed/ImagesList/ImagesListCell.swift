16c1522015720e2a4691d0e5fafa0adaeaabc3e6
Pavel Bobkov <pavel.bbkv1@gmail.com>
2024-02-03 21:16:51 +0300

    sprint 8 task

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Pavel on 01.02.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    static let reuseIdentifier = "ImagesListCell"
+}
