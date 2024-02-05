4e10d480c59b5bb991a63b8588f744dbcacec1de
Pavel Bobkov <pavel.bbkv1@gmail.com>
2024-02-05 18:02:33 +0300

    create ProfileViewContoller

---
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Pavel on 05.02.2024.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var UserImageView: UIImageView!
+    @IBAction func LogoutButton(_ sender: UIButton) {
+    }
+    @IBOutlet weak var LogoutButton: UIButton!
+    @IBOutlet weak var UserDescriptionLabel: UILabel!
+    @IBOutlet weak var UserLoginLabel: UILabel!
+    @IBOutlet weak var UserNameLabel: UILabel!
+}
